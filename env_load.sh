ae74327a9b044e4b2c91f116eea582748f1ac6e5
Alan Janis <alan.janis@gmail.com>
2020-09-28 20:59:47 -0600

    add debug output to ansible environment helper script

--- env_load.sh
+++ env_load.sh
@@ -1,10 +1,22 @@
 #!/bin/zsh
 export VAULTPATH=${0:a:h}
-echo "Ansible Working Path: $VAULTPATH"
 export ANSIBLE_VAULT_PASSWORD_FILE=${VAULTPATH}/.vault
-echo "Vault PW File: $ANSIBLE_VAULT_PASSWORD_FILE"
 export ANSIBLE_INVENTORY=${VAULTPATH}/hosts
+export ANSIBLE_CONFIG=${VAULTPATH}/ansible.cfg
+
+if (( ${+VAULTPATH} )); then
+echo "Ansible Working Path: $VAULTPATH"
+fi
+if (( ${+ANSIBLE_VAULT_PASSWORD_FILE} )); then
+echo "Vault PW File: $ANSIBLE_VAULT_PASSWORD_FILE"
+fi
+if (( ${+ANSIBLE_INVENTORY} )); then
 echo "Ansible Inventory File: $ANSIBLE_INVENTORY"
+fi
+if (( ${+ANSIBLE_CONFIG} )); then
+echo "Ansible Configuration File: $ANSIBLE_CONFIG"
+fi
+
 
 
 # Redis Fact Cache
@@ -27,3 +39,14 @@ export ANSIBLE_CACHE_PLUGIN_CONNECTION=fact_cache
 #export ANSIBLE_INVENTORY_CACHE_PLUGIN=redis
 #export ANSIBLE_INVENTORY_CACHE_PLUGIN_CONNECTION=10.0.10.152:6379
 #export ANSIBLE_INVENTORY_CACHE_PLUGIN_PREFIX=inventory_
+
+if (( ${+ANSIBLE_CACHE_PLUGIN} )); then
+echo "Ansible Cache Plugin: $ANSIBLE_CACHE_PLUGIN"
+fi
+if (( ${+ANSIBLE_CACHE_PLUGIN_CONNECTION} )); then
+echo "Ansible Cache Plugin Connection: $ANSIBLE_CACHE_PLUGIN_CONNECTION"
+fi
+if (( ${+ANSIBLE_CACHE_PLUGIN_PREFIX} )); then
+echo "Ansible Cache Plugin Connection: $ANSIBLE_CACHE_PLUGIN_PREFIX"
+fi
+
